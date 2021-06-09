f58231255ab200c411721724ed8816e6ec9c2554
João Lourenço <joaolourenco@MBP.Home>
2021-06-09 20:30:30 +0100

    Minor changer in Controller

--- src/proj/Controller.java
+++ src/proj/Controller.java
@@ -4,32 +4,34 @@ public class Controller {
     
     public void start() {
 
-        Menu mainMenu = new Menu("Jogar", "Consultar Equipa", "Consultar Jogador por nome", "Consultar Jogos", "Carregar ficheiro", "Salvar");
+        Menu mainMenu = new Menu("Simular Jogo", "Torneio", "Consultar Equipa", "Consultar Jogador por nome", "Consultar Jogos", "Carregar ficheiro", "Salvar");
         boolean sair = true;
 
         while(sair){
             mainMenu.executa();
 
             switch(mainMenu.getOpcao()) {
-                case(1): // Jogar
+                case(1): // Simular um jogo
 
-                case(2): // Consultar equipa
+                case(2): // Torneio
+
+                case(3): // Consultar equipa
                     ControllerEquipa.start(Parser.getEquipas());
                     break;
                 
-                case(3): // Consultar jogador
+                case(4): // Consultar jogador por nome
                     ControllerJogador.start(Parser.getEquipas());
                     break;
             
-                case(4): // Consultar jogos
+                case(5): // Consultar jogos
                     List<Jogo> jogos = new ArrayList<>(Parser.getJogos());
                     View.showJogos(jogos);
                     View.pressAnyKey();
                     break;
 
-                case(5): // Carregar ficheiro
+                case(6): // Carregar ficheiro
 
-                case(6): // Salvar
+                case(7): // Salvar
 
                 case(0): //Sair
                 sair = false;
